27c1294b6c713ded7415aef3dbae66b0247e9497
Rishabh Kumar <rishabh.kumar@resonate.com>
2024-05-28 07:22:19 -0400

    including setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='methods-mastery-assignment',
+    version='0.1',
+    packages=find_packages(),
+    author='Rishabh Kumar',
+    description='Github project for assignment',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/RishabhKumar777/MnM-assignment',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
