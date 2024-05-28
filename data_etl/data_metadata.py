76efcdd31688c08878904afd333f97738945234e
Rishabh Kumar <rishabh.kumar@resonate.com>
2024-05-28 07:37:14 -0400

    including setup.py 5

--- data_etl/data_metadata.py
+++ data_etl/data_metadata.py
@@ -1,5 +1,6 @@
 from sqlalchemy import MetaData, Table, Column, Integer, String
-# Define the schema for the metadata being used in data_loader.py
+# Define the schema for the metadata being used in data_loader.py. This is a file which is not being used
+# but at any point of time can be utilized to change the schema of data in future
 metadata = MetaData()
 posts = Table(
     'posts', metadata,
